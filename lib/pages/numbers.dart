ee915044aa286053f4c9d481a30b31dfe1d5d050
Fretchel <fretchelm.gerarman@gmail.com>
2023-03-30 20:19:47 +0800

    updated the list kaduha

--- lib/pages/numbers.dart
+++ lib/pages/numbers.dart
@@ -20,7 +20,7 @@ class _NumbersPageState extends State<NumbersPage> {
     Details(
         name: 'Alburquerque Rural Health Unit',
         address: 'Alburquerque, Bohol',
-        number: "(038) 539-9223/(0999) 586-6464"),
+        number: "(0999) 586-6464"),
     Details(
         name: 'Alicia Rural Health Unit and Birthing Center',
         address: 'POBLACION (CALINGGANAY), Alicia, Bohol',
@@ -36,7 +36,7 @@ class _NumbersPageState extends State<NumbersPage> {
     Details(
         name: 'BACLAYON RURAL HEALTH UNIT',
         address: 'Poblacion, Baclayon, Bohol',
-        number: '(038) 540-9280/(0921) 214-0029'),
+        number: '(0921) 214-0029'),
     Details(
         name: 'BALILIHAN MUNICIPAL HEALTH OFFICE',
         address: 'Del Carmen Este, Balilihan, Bohol',
@@ -44,7 +44,7 @@ class _NumbersPageState extends State<NumbersPage> {
     Details(
         name: 'BATUAN RURAL HEALTH UNIT',
         address: 'POBLACION SUR, Batuan, Bohol',
-        number: '(038) 533-9546/(0946) 626-9003'),
+        number: '(0946) 626-9003'),
     Details(
         name: 'BIEN UNIDO RURAL HEALTH UNIT',
         address: 'POBLACION (BIEN UNIDO), Bien Unido, Bohol',
@@ -52,11 +52,11 @@ class _NumbersPageState extends State<NumbersPage> {
     Details(
         name: 'BILAR MUNICIPAL HEALTH OFFICE',
         address: 'POBLACION, Bilar, Bohol',
-        number: '(0920) 981-3608/ (038) 535-0012'),
+        number: '(038) 535-0012'),
     Details(
         name: 'BUENAVISTA RURAL HEALTH UNIT',
         address: 'POBLACION, Buenavista, Bohol',
-        number: '(0966) 834-4553/(0947) 201-4784'),
+        number: '(0947) 201-4784'),
     Details(
         name: 'Calape Mother and Child Hospital, Calape, Bohol',
         address: 'Tagbilaran North Road, Calape, Bohol',
@@ -64,7 +64,7 @@ class _NumbersPageState extends State<NumbersPage> {
     Details(
         name: 'Candijay Community Hospital',
         address: 'POBLACION, Candijay, Bohol',
-        number: '+63 38 562 0022, +63 385108292, 0915 541 3569'),
+        number: '0915 541 3569'),
     Details(
         name: 'IMAP LYING-IN CLINIC, INC',
         address: 'Purok 4, Barangay Tangoan, Carmen Bohol Philippines',
@@ -72,7 +72,7 @@ class _NumbersPageState extends State<NumbersPage> {
     Details(
         name: 'CATIGBIAN – MAIN HEALTH CENTER AND LYING IN',
         address: 'POBLACION WESTE, Catigbian, Bohol',
-        number: '(038) 416-2326/(0920) 918-1327'),
+        number: '(0920) 918-1327'),
     Details(
         name: 'Clarin Community Hospital',
         address: 'X279+G2X, Bonbon Barangay Rd, Clarin, Bohol',
